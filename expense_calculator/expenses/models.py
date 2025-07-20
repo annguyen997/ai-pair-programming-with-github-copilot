2e80d80f6549d476ae148451d804c5289a893146
An Nguyen <anguye60@gmu.edu>
2025-07-20 16:46:58 -0400

    Initial commit

---
+++ expense_calculator/expenses/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Expense(models.Model):
+    """
+    Expense model
+    """
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - ${self.amount} on {self.date}"
+
