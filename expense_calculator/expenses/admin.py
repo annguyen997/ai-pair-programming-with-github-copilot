801361029b5f696c89bdb7dbc3fc4d50bc4eb2a2
An Nguyen <anguye60@gmu.edu>
2025-07-20 16:52:11 -0400

    New repository for learning project

--- expense_calculator/expenses/admin.py
+++ expense_calculator/expenses/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Expense
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'amount', 'timestamp', 'category')
+    search_fields = ('name', 'category__name')
+    list_filter = ('category', 'timestamp')
+    ordering = ('-timestamp',)
+
+    
--- expense_calculator/expenses/models.py
+++ expense_calculator/expenses/models.py
@@ -9,7 +9,7 @@ class Expense(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True)
+    # date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name} - ${self.amount} on {self.date}"
